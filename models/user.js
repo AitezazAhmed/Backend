990cafa5413e07fe305354b921b73dc4df688ec6
AitezazAhmed <stoneghold4103@gmail.com>
2025-04-09 00:04:14 +0500

    user.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,15 @@ const mongoose=require("mongoose")
 const userSchema= new mongoose.Schema({
     username:String,
     email:String, 
-   password:String
+   password:String,
+   role: { type: String, enum: ["user", "admin"], default: "user" },
+   uploadedFiles: [
+    {
+        filename: String,
+        filePath: String,
+        uploadedAt: { type: Date, default: Date.now }
+    }
+]
 })
 const userModel=mongoose.model("user",userSchema)
 module.exports=userModel
